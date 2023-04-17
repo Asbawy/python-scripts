2b4724eef12ddabff56e33eca97a1bdc2ed34009
Mhmmd Ashraf <medo.elkasaby55@gmail.com>
2023-04-17 02:18:26 +0200

    validate the user input

--- domainChecker.py
+++ domainChecker.py
@@ -1,5 +1,15 @@
 import requests
 import sys
+import os.path
+
+def validate_domain_name(domain_name):
+    if ' ' in domain_name:
+        return False
+    elif not domain_name:
+        return False
+    else: 
+        return True
+    
 
 def check_status_code(domain):
     try:
@@ -7,36 +17,58 @@ def check_status_code(domain):
         print(f"{domain} -- Status code : {response.status_code}")
     except requests.exceptions.RequestException as e:
         print(f"{domain} -- Error: {e}")
+        print("Please check the domain name and try again.")
 
 
 def check_multiple_domains(file_path):
-    with open(file_path, 'r') as f:
-        domains = f.read().splitlines()
-        for domain in domains:
-            check_status_code(domain)
+    try:
+        with open(file_path, 'r') as f:
+            domains = f.read().splitlines()
+    except FileNotFoundError:
+            print(f"Error: Could not find the file '{file_path}'")
+            return
+    
+    for domain in domains:
+        check_status_code(domain)
+
     sys.exit()
 
 def main():
-    print("\n*** Welcome to the Domain Status Checker ***\n")
+    print("*** Welcome to the Domain Status Checker ***\n")
+
     while True:
         print("Please choose one of the following options:")
-        print("1. Check status code for a singe domain")
+        print("1. Check status code for a single domain")
         print("2. Check status code for multiple domains in a file")
         print("3. Exit")
+
         choice = input("\nEnter your choice (1, 2, 3): ")
 
         if choice == "1":
-            domain = input("\nEnter the domain name: ")
+            while True:
+                domain = input("\nEnter the domain name: ")
+                if validate_domain_name(domain):
+                    break
+                else:
+                    print("Invalid domain name. Please try again.")
+
             check_status_code(domain)
             sys.exit()
         elif choice == "2":
-            file_path = input("Enter the file path: ")
+            while True:
+                file_path = input("\nEnter the file path: ")
+                if os.path.isfile(file_path):
+                    break
+                else:
+                    print("Invalid file path. Please try again.")
+
             check_multiple_domains(file_path)
         elif choice == "3":
-            print("\nThanks you fo using the Domain Status Checker By:Anubis!")
+            print("\nThank you for using the Domain Status Checker By:Anubis!")
             sys.exit()
         else:
-            print("\nInvalid choice, Please choose a valid option")
+            print("\nInvalid choice. Please choose a valid option.\n")
             continue
+
 if __name__ == '__main__':
     main()
