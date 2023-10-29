699b1f3df76988a0d1d6a9381902b1af68e1dd4f
Mhmmd Ashraf <medo.elkasaby55@gmail.com>
2023-10-29 02:34:41 +0200

    fast tool to brute jwt tokens

--- JWT_Brute/jwt_brute.py
+++ JWT_Brute/jwt_brute.py
@@ -4,42 +4,55 @@ import sys
 from colorama import Fore
 import time
 from concurrent.futures import ThreadPoolExecutor
+import re
+from tqdm import tqdm
 
-def brute_force_jwt(encoded, secret):
-    try:
-        payload = jwt.decode(encoded, secret, algorithms=['HS256'])
-        return secret
-    except jwt.InvalidKeyError:
-        return None
-    except jwt.ExpiredSignatureError:
-        return None
-    except Exception as e:
-        return None
+def brute_force_jwt(encoded, secrets):
+    success = None
+    for secret in secrets:
+        try:
+            payload = jwt.decode(encoded, secret, algorithms=['HS256'])
+            return secret
+            break
+        except jwt.InvalidKeyError:
+            pass
+        except jwt.ExpiredSignatureError:
+            pass
+        except Exception as e:
+            pass
+    return success
 
 def main():
-    print(Fore.RED+"\nScript for brute force jwt token by: Asbawy")
+    banner = """
+    ╔╦╗╔═╗╔╦╗ ╦╦ ╦╔╦╗
+     ║║║╣  ║║ ║║║║ ║
+    ═╩╝╚═╝═╩╝╚╝╚╩╝ ╩ v1.0
+    JWT Bruter by Asbawy
+    """
+    print(Fore.RED+banner)
     encoded = input(Fore.BLUE+"Enter JWT token: ")
     password_list = input(Fore.BLUE+"Enter the passwords list: ")
 
     try:
         with open(password_list) as secrets_file:
             secrets = [line.strip() for line in secrets_file]
+        
+        num_threads = 4  # Adjust this number based on your system's capabilities
+        batch_size = len(secrets) // num_threads
+
         print(Fore.BLUE+f"[INFO] Starting brute force with {len(secrets)} passwords....")
         start_time = time.time()
         success = None
         passwords_tested = 0
 
-        with ThreadPoolExecutor(max_workers=4) as executor:
-            for secret in secrets:
-                result = executor.submit(brute_force_jwt, encoded, secret)
-                passwords_tested += 1
+        with ThreadPoolExecutor(max_workers=num_threads) as executor:
+            batches = [secrets[i:i + batch_size] for i in range(0, len(secrets), batch_size)]
+            for batch in tqdm(batches, unit=' batch', ncols=100, ascii=True):
+                result = executor.submit(brute_force_jwt, encoded, batch)
                 if result.result():
                     success = result.result()
                     break
-                    
-                sys.stdout.write(Fore.BLUE+f"\r[INFO] Passwords tested: {passwords_tested}")
-                sys.stdout.flush()
-        
+
         if success:
             print(Fore.GREEN+f"\n[+] Token decoded with the following password: [{success}]")
         else:
